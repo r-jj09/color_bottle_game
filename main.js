e08f85a100aef38987a3620d4ce45b2ce3977a15
hajdur <hajdu.reka1109@gmail.com>
2023-10-20 12:12:40 +0200

    something that is visible

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// $("body").on("click", "button", (event) => {
-// 	let form = $("form");
-// 	let message = $(".message");
-// 	const formData = new FormData(form.get(0));
-// 	formData.append("message", message.text());
-// 	for (const [key, value] of formData.entries()) {
-// 		console.log(`${key} ${value}`);
-// 	}
-// });
-
 class BaseBottle {
 	numerOfColors = 4;
 	baseColors = [
@@ -19,24 +9,68 @@ class BaseBottle {
 		"maroon",
 		"green",
 		"yellow",
+		"teal",
+		"aqua",
 	];
 	colors = [];
 	constructor() {
-		// itt kellene feltölteni a colors paramétert.
 		for (let i = 0; i < this.numerOfColors; i++) {
 			const randomIndex = Math.floor(Math.random() * this.baseColors.length);
 			this.colors.push(this.baseColors[randomIndex]);
 		}
+		this.showBottle();
 	}
 
 	showBottle() {
-		// itt jelenítsd meg-
 		var bottle = document.createElement("div");
 		bottle.classList.add("bottle");
-		var box = document.createElement("div");
-		box.classList.add("box");
-		bottle.append(box);
+		for (let i = 0; i < this.numerOfColors; i++) {
+			var box = document.createElement("div");
+			box.classList.add("box");
+			box.style.backgroundColor = this.colors[i];
+			bottle.append(box);
+			$("body").append(bottle);
+		}
 	}
 }
-const bottle = new BaseBottle();
-console.log(bottle.colors);
+class Playground {
+	rowsCount;
+	colsCount;
+	field = $(
+		"<div style='width: 20px; height: 80px;border: 1px solid black; display: inline-flex; flex-direction: column; position: relative;'></div>"
+	);
+
+	constructor(rowsCount = 10, colsCount = 10) {
+		this.rowsCount = rowsCount;
+		this.colsCount = colsCount;
+		this.showPlayground();
+	}
+
+	showPlayground() {
+		for (let i = 0; i < this.rowsCount; i++) {
+			for (let j = 0; j < this.colsCount; j++) {
+				let id = i * this.colsCount + j;
+				let clone = this.field.clone();
+
+				clone.attr("id", id);
+				$("body").append(clone);
+			}
+			$("body").append("<br>");
+		}
+	}
+}
+
+class Game {
+	playground;
+
+	constructor(playground) {
+		this.playground = playground;
+		this.startGame();
+	}
+
+	startGame() {
+		//
+	}
+}
+
+game = new Game(new Playground(2, 5), new BaseBottle());
